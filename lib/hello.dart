42fe199374db1aa4cd0bf661204f9a791ce971c6
Arjun Verma <kshatriya.arjun.developer@gmail.com>
2023-04-10 14:16:09 +0530

    FEAT: Add extension for onclick without splash effect

--- lib/hello.dart
+++ lib/hello.dart
@@ -101,9 +101,17 @@ StatelessWidget PaddingCustom({
     );
 
 extension ClickExtension on Widget {
-  /// Adds on click without splash effect.
+  /// Adds on click with splash effect.
   Widget onClickSplash(void Function() onClickFunction) {
     return InkWell(
+      onTap: onClickFunction,
+      child: this,
+    );
+  }
+
+  /// Adds on click without splash effect.
+  Widget onClick(void Function() onClickFunction) {
+    return GestureDetector(
       onTap: onClickFunction,
       child: this,
     );
