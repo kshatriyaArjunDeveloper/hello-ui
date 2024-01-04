d8d99e810147d4f47600cc14064f2d8aa5c106a6
Arjun Verma <kshatriya.arjun.developer@gmail.com>
2024-01-04 23:47:36 +0530

    FEAT: image loading class for svg

--- lib/hello.dart
+++ lib/hello.dart
@@ -4,4 +4,5 @@ export 'src/ui/helper/clicks/click_extension.dart';
 export 'src/ui/helper/styling/style_string_extension.dart';
 export 'src/ui/helper/system_ui/system_ui_helper.dart';
 export 'src/ui/widget/container.dart';
+export 'src/ui/widget/image.dart';
 export 'src/ui/widget/padding.dart';
---
+++ lib/src/ui/widget/image.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class Image_ extends StatelessWidget {
+  final double? height;
+  final double? width;
+
+  // SVG related
+  final String? localSvgAsset;
+  final Color? svgColor;
+  final Gradient? iconGradient;
+
+  const Image_({
+    super.key,
+    this.localSvgAsset,
+    this.height,
+    this.width,
+    this.svgColor,
+    this.iconGradient,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      child: iconGradient != null
+          ? _maskWithGradient(buildSvgIcon(isMaskingGradient: true))
+          : buildSvgIcon(),
+    );
+  }
+
+  Widget _maskWithGradient(
+    Widget child,
+  ) =>
+      ShaderMask(
+        shaderCallback: (bounds) => iconGradient!.createShader(bounds),
+        child: child,
+      );
+
+  Widget buildSvgIcon({bool isMaskingGradient = false}) {
+    return SvgPicture.asset(
+      localSvgAsset!,
+      height: height,
+      width: width,
+      fit: BoxFit.contain,
+      color: isMaskingGradient ? Colors.white : svgColor,
+    );
+  }
+}
